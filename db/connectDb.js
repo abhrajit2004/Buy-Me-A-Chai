a7acbede381c8d22c0bb708ee341bfcb58396e6c
unknown <abhrajitgupta.it2022@nsec.ac.in>
2024-08-26 15:17:46 +0530

    added mongo_uri

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
+      const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
         useNewUrlParser: true,
       });
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
+      console.log(`MongoDB Connected: conn.connection.host`);
     } catch (error) {
       console.error(error.message);
       process.exit(1);
