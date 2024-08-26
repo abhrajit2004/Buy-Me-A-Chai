cdb0ad2e7d8c59d843155b7fa14729292349aa71
unknown <abhrajitgupta.it2022@nsec.ac.in>
2024-08-26 23:44:10 +0530

    added mongodb uri for cloudinary

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-      const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
-        useNewUrlParser: true,
-      });
+      const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
       console.log(`MongoDB Connected: conn.connection.host`);
     } catch (error) {
       console.error(error.message);
